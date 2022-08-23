d792cc9110b955c69aee019c9a0cf0184a3a4f9d
Ivan Castellanos <ivan@ivant.dev>
2022-08-23 18:14:20 -0500

    [ADD] clase abstracta de stack generico
    [ADD] clase de array generico
    
    [ADD] clase stack palindromo
    
    [ADD] clase stack postfix

--- stack/stack.py
+++ stack/stack.py
@@ -1,43 +1,123 @@
-class Stack():
-  def __init__(self, size=5):
-    self.max = size
-    self.arr = [None]*self.max
-    self.idx = 0
+from abc import ABC, abstractmethod
+from curses.ascii import isdigit
 
-  def full(self):
-    return self.max == self.idx
+class GenericStack(ABC):
+    @abstractmethod
+    def push(self, x):
+        pass
 
-  def empty(self):
-    return self.idx == 0
+    @abstractmethod
+    def pop(self):
+        pass
+    
+    @abstractmethod
+    def full(self):
+        pass
+    
+    @abstractmethod
+    def empty(self):
+        pass
 
-  def push(self, x):
-    if (not self.full()):
-      self.arr[self.idx] = x
-      self.idx += 1
-      return
-    else:
-      print('full')
+class GenericArrayStack(GenericStack):
+    def __init__(self, size = 5):
+        self.__arr = [None]*size
+        self.__top = 0
+        self.__size = size
+    
+    @property
+    def arr(self):
+        return self.__arr
+    
+    @property
+    def top(self):
+        return self.__top
+        
+    def full(self):
+        return self.__top == self.__size
+    
+    def empty(self):
+        return self.__top <= 0
+    
+    def push(self, x):
+        if(not self.full()):
+            self.__arr[self.__top] = x
+            self.__top += 1
+        else: print('full')
+    
+    def pop(self):
+        if(not self.empty()):
+            self.__top -= 1
+            return self.__arr[self.__top]
+        else: print("empty")
 
-  def pop(self):
-    if(not self.empty()):
-      self.idx -= 1
-      x = self.arr[self.idx]
-      return x
+class PalindromeStack(GenericArrayStack):
+    def __init__(self, size=5):
+        super().__init__(size)
 
-  def is_palindrome(self):
-    palindrome = True
-    for i in range(self.idx//2):
-      if(self.arr[i] != self.arr[self.idx - 1 - i]):
-        palindrome = False
-      if(not palindrome): break
-    return palindrome
+    def is_palindrome(self):
+        palindrome = True
+        for i in range(self.top//2):
+            if(self.arr[i] != self.arr[self.top - 1 - i]):
+                palindrome = False
+            if(not palindrome): break
+        return palindrome
 
-  def getArr(self):
-    return self.arr
+    def getArr(self):
+        return self.arr
+    
+class PostfixStack(GenericArrayStack):
+    def __init__(self, size=5):
+        super().__init__(size)
+    
+    def push(self, x):
+        if isdigit(x):
+            x = int(x)
+            super().push(x)
+        elif(x == "+"):
+            a = self.pop()
+            b = self.pop()
+            c = a + b
+            super().push(c)
+        elif(x == "-"):
+            a = self.pop()
+            b = self.pop()
+            c = a - b
+            super().push(c)
+        elif(x == "*"):
+            a = self.pop()
+            b = self.pop()
+            c = a * b
+            super().push(c)
+        elif(x == "/"):
+            a = self.pop()
+            b = self.pop()
+            c = a / b
+            super().push(c)
+    
+    def getArr(self):
+        return super().__arr
+            
 
-string = "oso"
-stack = Stack(len(string))
-for i in string:
-  stack.push(i)
+def usePalindromeStack():
+    string = "oso"
+    stack = PalindromeStack(len(string))
+    for i in string:
+        stack.push(i)
+    print('array: ', stack.getArr(),'\nes palindromo?', stack.is_palindrome())
 
-print('array: ', stack.getArr(),'\nes palindromo?', stack.is_palindrome())
+def usePostFixStack(s):
+    s = s.split()
+    stack = PostfixStack(len(s))
+    for i in s:
+        stack.push(i)
+    print('resultado: ', stack.pop())
+    print(stack.getArr())
+        
+def main():
+    # usePalindromeStack()
+    usePostFixStack("2 3 5 * +")
+    
+    usePostFixStack("2 3 + 5 *")
+    
+if __name__ == "__main__":
+    main()
